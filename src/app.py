09cfbc8bcbddf1e50563040ce59f4e0a7b750f42
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2024-09-14 01:59:17 +0000

    :tada: Our first CRUD!

--- src/app.py
+++ src/app.py
@@ -36,14 +36,76 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# READ - query.all()
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    users  = User.query.all()
+    usuarios_serializados = [ persona.serialize() for persona in users ]
+    return jsonify(usuarios_serializados), 200
 
-    return jsonify(response_body), 200
+# CREATE - save()
+@app.route('/user', methods=['POST'])
+def add_user():
+
+    body = request.json
+
+    username = body.get('username', None)
+    email = body.get('email', None)
+    password = body.get('password', None)
+
+    if username == None or email == None or password == None:
+        return jsonify({ "msg" : "Missing fields"}), 400 
+
+    try:
+        new_user = User(email=email, username=username, password=password)
+
+        db.session.add(new_user) # Memoria RAM
+        db.session.commit() # Se guarda con la instruccion SQL
+
+        return jsonify({ "msg" : "success"}), 201
+
+    except:
+        return jsonify({ "error": "Something happened unexpectedly" }), 500
+
+# DELETES a row
+@app.route('/user/<string:username>', methods=['DELETE'])
+def remove_user(username):
+
+    searched_user = User.query.filter_by(username=username).one_or_none()
+
+    if searched_user != None:
+        db.session.delete(searched_user)
+        db.session.commit()
+        return jsonify(searched_user.serialize()), 200
+    else:
+        return jsonify({ "error": f"User with username: {username} not found!" }), 404
+
+    # return jsonify({ "error": "Something happened unexpectedly" }), 500
+
+# UPDATE an Object or Row in table
+@app.route('/user/<string:username>', methods=['PUT'])
+def update_user(username):
+
+    searched_user = User.query.filter_by(username=username).one_or_none()
+
+    body = request.json
+    new_username = body.get('username', None)
+    password = body.get('password', None)
+
+    if searched_user != None:
+        
+        if new_username != None:
+            searched_user.username = new_username
+
+        if password != None:
+            searched_user.password = password
+
+
+        db.session.commit()
+        return jsonify(searched_user.serialize()), 200
+    else:
+        return jsonify({ "error": f"User with username: {username} not found!" }), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -5,15 +5,23 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(12), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    def __init__(self, email, password, username):
+        self.email = email
+        self.password = password
+        self.username = username
+        self.is_active = True
+
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Usuario %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            # do not serialize the password, its a security breach,
+            "username": self.username
         }
